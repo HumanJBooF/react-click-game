4016db08deb98e3b4a7124900e68471ff52c5b6f
humanjboof <humanjboof@gmail.com>
2018-10-28 19:53:53 -0400

    Updates

--- precache-manifest.09fc6262ecff112133bcc37b286bb619.js
+++ precache-manifest.09fc6262ecff112133bcc37b286bb619.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-click-game/static/js/runtime~main.39ce7167.js"
   },
   {
-    "revision": "0603c513a45d9e22f458",
-    "url": "/react-click-game/static/js/main.0603c513.chunk.js"
+    "revision": "b633a9835f2f8f668180",
+    "url": "/react-click-game/static/js/main.b633a983.chunk.js"
   },
   {
     "revision": "f843a591dc3acd9073f0",
     "url": "/react-click-game/static/js/1.f843a591.chunk.js"
   },
   {
-    "revision": "1736ed7eacde1091564db5d00e4b9161",
+    "revision": "ee95b87c3f3b330be2f39581fd55ecbd",
     "url": "/react-click-game/index.html"
   }
 ];
